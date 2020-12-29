bccd0a7aaf4e8108613b8cf2de85088da12b62f9
neithaltair <neith.altair.na@gmail.com>
2020-12-29 12:50:17 -0500

    CREATE= Cadenas y entradas python

---
+++ introduccion/cadenasyEntradas.py
@@ -0,0 +1,24 @@
+#Cadenas y entradas con python
+
+my_str = 'Neith Altair'
+
+#Imprime el contenido de la variable
+print(my_str)
+print()
+#Imprime posiciones especificas
+print(my_str[0])
+print(my_str[2])
+print(my_str[5])
+print()
+#Imprime desde N hasta el final
+print(my_str[2:])
+print()
+#Imprime desde el inicio hasta N
+print(my_str[:5])
+print()
+#Imprime desde N hasta el inicio
+print(my_str[:-3])
+print()
+#Imprime desde el inicio hasta el fin de 2 en 2
+print(my_str[::2])
+
